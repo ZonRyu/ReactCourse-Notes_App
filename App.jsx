634dd44a03e967ca9c91147af980b2f4a6ed4ee5
ZonRyu <julyonchang@gmail.com>
2024-08-04 22:43:54 +0700

    change default tempNoteText value

--- App.jsx
+++ App.jsx
@@ -8,7 +8,7 @@ import { notesCollection, db } from "./firebase"
 export default function App() {
     const [notes, setNotes] = React.useState([])
     const [currentNoteId, setCurrentNoteId] = React.useState("")
-    const [tempNoteText, setTempNoteText] = React.useState([])
+    const [tempNoteText, setTempNoteText] = React.useState("")
     
     const currentNote = 
         notes.find(note => note.id === currentNoteId) 
